9c4fd054ca1c1385cb0e02a94a0d10575f409e16
Ridhi Priya <106872423+Ridhipriya85@users.noreply.github.com>
2024-02-17 13:21:14 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,17 @@ import './App.css'
 import Navbar from './components/navbar/Navbar'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './pages/home/Home';
+import About from './pages/home/about/About';
 
 function App() {
   return (
     <>
 
      <BrowserRouter>
-     <Navbar/>
+     {/* <Navbar/> */}
       <Routes>
-        <Route path="/" element={<Home />}>
+        {/* <Route path="/" element={<Home />}> */}
+        <Route path="/" element={<About />}>
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ src/pages/home/about/About.jsx
@@ -0,0 +1,10 @@
+
+
+export default function About() {
+  return (
+    <>
+    <div>jsdj</div>
+    <h1 className='text-light mt-5 pt-4 mx-4 fw-bold text-center'>Our Story</h1>
+    </>
+  )
+}
