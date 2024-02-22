36ae908e4038af6a7d4ca4bd36ea8419faf11bed
SHIVAM KUMAR <shivamjha2705@gmail.com>
2024-02-22 13:50:22 +0530

    add link routes to the specific pages

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -51,12 +51,12 @@ export default function Navbar(props) {
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item mx-3">
-                                <Link className="nav-link   active " aria-current="page" to="#">
+                                <Link className="nav-link   active " aria-current="page" to="/">
                                     Home
                                 </Link>
                             </li>
                             <li className="nav-item mx-3">
-                                <Link className="nav-link  sm " to="#">
+                                <Link className="nav-link  sm " to="/existingPackage">
                                     Existing Packages
                                 </Link>
                             </li>
