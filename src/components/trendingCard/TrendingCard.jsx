cc639338d0d8daa3857edbf37261613e87d74602
SHIVAM KUMAR <shivamjha2705@gmail.com>
2024-02-22 15:28:55 +0530

    make border none and object fit image to cover

--- src/components/trendingCard/TrendingCard.jsx
+++ src/components/trendingCard/TrendingCard.jsx
@@ -3,23 +3,23 @@ import './TrendingCard.css'
 export default function TrendingCard({ trendingCardData }) {
   return (
     <div className="container">
-        <div className="row  d-flex justify-content-center ">
-            
+      <div className="row  d-flex justify-content-center ">
+
         {trendingCardData &&
-        trendingCardData.map((cardData, index) => (
-          <div className="card  px-0 my-3 mx-3 trending" style={{ width: "18rem"}} key={index}>
-            <img src={cardData.image} className="card-img-top" alt={cardData.alt} style={{height:"180px"}} />
-            <div className="card-body d-flex justify-content-between align-items-center">
-              <div className="d-flex justify-content-between flex-column ">
-                <h5 className="card-title" >{cardData.title}</h5>
-                <p className="card-text">{cardData.description}</p>
+          trendingCardData.map((cardData, index) => (
+            <div className="card  px-0 my-3 mx-3 trending border-0" style={{ width: "18rem" }} key={index}>
+              <img src={cardData.image} className="card-img-top object-fit-cover" alt={cardData.alt} style={{ height: "180px" }} />
+              <div className="card-body d-flex justify-content-between align-items-center">
+                <div className="d-flex justify-content-between flex-column ">
+                  <h5 className="card-title" >{cardData.title}</h5>
+                  <p className="card-text">{cardData.description}</p>
+                </div>
+                <FaArrowRightLong />
               </div>
-              <FaArrowRightLong />
             </div>
-          </div>
-        ))}
-        </div>
-      
+          ))}
+      </div>
+
     </div>
   );
 }
